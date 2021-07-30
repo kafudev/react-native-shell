8a547f551bfbae08b53c71e56d32ae4c5a2f239f
zpd106 <zpd106@sina.com>
2021-07-30 19:59:36 +0800

    chore: fix bug and remove expo-location

--- android/app/src/main/java/com/rnshell/app/generated/BasePackageList.java
+++ android/app/src/main/java/com/rnshell/app/generated/BasePackageList.java
@@ -22,7 +22,6 @@ public class BasePackageList {
         new expo.modules.imageloader.ImageLoaderPackage(),
         new expo.modules.imagepicker.ImagePickerPackage(),
         new expo.modules.keepawake.KeepAwakePackage(),
-        new expo.modules.location.LocationPackage(),
         new expo.modules.permissions.PermissionsPackage(),
         new expo.modules.sensors.SensorsPackage(),
         new expo.modules.splashscreen.SplashScreenPackage(),
