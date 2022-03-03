20366159f1e9fb93fde909a32ff9c6c5f683a585
jude <zpd106@sina.com>
2022-03-03 22:03:48 +0800

    up dep

--- android/app/src/main/java/com/rnshell/app/jsi/CommonJSIModulePackage.java
+++ android/app/src/main/java/com/rnshell/app/jsi/CommonJSIModulePackage.java
@@ -6,6 +6,8 @@ import com.facebook.react.bridge.JavaScriptContextHolder;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 import com.nozbe.watermelondb.jsi.WatermelonDBJSIPackage; // ⬅️ This!
+import com.reactnativemmkv.MmkvModule; // react-native-mmkv
+import com.reactnativemmkv.MmkvModulePackage; // react-native-mmkv
 
 import java.util.Collections;
 import java.util.List;
@@ -19,6 +21,7 @@ public class CommonJSIModulePackage extends  ReanimatedJSIModulePackage {
       List<JSIModuleSpec> modules = Arrays.asList();
       // modules.addAll(new ReanimatedJSIModulePackage().getJSIModules(reactApplicationContext, jsContext));
       modules.addAll(new WatermelonDBJSIPackage().getJSIModules(reactApplicationContext, jsContext)); // ⬅️ This!
+      modules.addAll(new MmkvModulePackage().getJSIModules(reactApplicationContext, jsContext));
       return modules;
     }
 }
